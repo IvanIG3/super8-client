c667776133507aaf61cd4d4ad24592d6df651d26
IvanIG3 <ivaniglesias3gh@gmail.com>
2020-12-02 11:46:29 +0100

    init redux provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.scss';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Super8</title>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Rammetto+One&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
+        </>
+    )
 }
 
 export default MyApp
