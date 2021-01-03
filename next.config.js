83f9f7c84c26d0e0d7a4b883277f3ffb8e0bd005
IvanIG3 <ivaniglesias3gh@gmail.com>
2021-01-04 00:13:13 +0100

    reajusted image sizes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     env: {
         tmdbURL: 'https://api.themoviedb.org/3',
-        tmdbImageURL: 'https://image.tmdb.org/t/p/w500',
-        tmdbBackdropURL: 'https://image.tmdb.org/t/p/w1280',
+        tmdbImageURL: 'https://image.tmdb.org/t/p/w342',
+        tmdbBackdropURL: 'https://image.tmdb.org/t/p/w780',
         tmdbProfileURL: 'http://image.tmdb.org/t/p/h632',
         defaultLanguage: 'es-ES',
     },
